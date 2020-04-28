e24a69e7908be517fd539e7b9a0b9fba1ce31d97
Paul Pan <panjunhong05@gmail.com>
2020-04-28 00:58:38 +0000

    Milestone changes

--- todolist/mainsite.py
+++ todolist/mainsite.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from datetime import datetime
+from .forms import TodoForm, DeleteForm
 
 bp = Blueprint('mainsite', __name__, url_prefix='/')
 
-@bp.route('/')
+todolist = [{"item": "abcd", "type": "todo", "description": "oaisdfasf"}]
+
+@bp.route('/', methods=['GET', 'POST', 'DELETE'])
 def hello():
-    return render_template('base.html')
+    print(request.method)
+    form = TodoForm()
+    deleteForm = DeleteForm()
+    if request.method == 'POST':
+        if request.form.get('submit') == 'Add Item':
+          todolist.append({"item": form.item.data, "type": form.type.data, "description": form.description.data})
+        elif len(todolist) != 0:
+          todolist.pop()
+    return render_template('todolist.html', todolist=todolist, datetime=datetime, form=form, deleteForm=deleteForm)
